c5b7d5dc8d1da6015cd09d62e93ecf2717cfacc6
AdarshSingh1705 <as.adarshsingh13@gmail.com>
2026-09-17 22:21:33 +0530

    Improving health check to more flexible and modular Creating Database tables along also inserting some values in it

--- api-management-service/src/main/java/in/adarshsingh/gatekeeper/management/ApiManagementServiceApplication.java
+++ api-management-service/src/main/java/in/adarshsingh/gatekeeper/management/ApiManagementServiceApplication.java
@@ -2,9 +2,8 @@ package in.adarshsingh.gatekeeper.management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@SpringBootApplication
 public class ApiManagementServiceApplication {
 
 	public static void main(String[] args) {
--- api-management-service/src/main/java/in/adarshsingh/gatekeeper/management/controller/HealthController.java
+++ api-management-service/src/main/java/in/adarshsingh/gatekeeper/management/controller/HealthController.java
@@ -1,29 +1,29 @@
 package in.adarshsingh.gatekeeper.management.controller;
 
 import in.adarshsingh.gatekeeper.management.dto.responseDto.HealthResponse;
-import org.springframework.beans.factory.annotation.Value;
+import in.adarshsingh.gatekeeper.management.service.HealthService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+
 @RestController
 @RequestMapping("/health")
-public class HealthController {
+public class   HealthController {
 
-    private String serviceName;
-    private String serviceDescription;
+    public final HealthService healthService;
 
-    public HealthController(@Value("${info.app.name}") String serviceName,
-                            @Value("${info.app.description}") String serviceDescription) {
-        this.serviceName = serviceName;
-        this.serviceDescription = serviceDescription;
+    public HealthController(HealthService healthService) {
+        this.healthService = healthService;
     }
 
     @GetMapping
     public ResponseEntity<HealthResponse> health() {
+        String dbStatus = healthService.getHealth();
         return ResponseEntity.ok(
-                new HealthResponse(serviceName, serviceDescription, "UP"));
+                new HealthResponse("UP", dbStatus, LocalDateTime.now()));
     }
 
 }
